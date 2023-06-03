f4622da9bac0217c3f8c2c8c3c2586567534f48d
Gustavo Novaro <gnovaro@gmail.com>
2023-06-03 14:06:19 +0200

    refactor(global): Change youtube-dl for yt-dlp
    Adding bootstrap

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# youtubedownloaderphp
+# Youtube Downloader PHP
 Make your own YouTube Downloader Mp3 Site
 
 ## Requisites
 * Linux
 * php
 * Web server
-* youtube-dl
-* ffmpeg
+* yt-dlp  (https://github.com/yt-dlp/yt-dlp)
+* ffmpeg (https://ffmpeg.org/)
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Author: Gustavo Novaro
- * @version 1.0.5
+ * @version 2.0.0
  * Youtube2Mp3 Downloader page
  *
  * Requisites
- * youtube-dl
- * https://rg3.github.io/youtube-dl/download.html
+ * yt-dlp
+ * https://github.com/yt-dlp/yt-dlp
  *
  * ffmpeg
  */
@@ -14,7 +14,7 @@
 if(!empty($_POST['url']))
 {
     $url = trim($_POST['url']);
-    $command_filename = "youtube-dl $url -f 140 --get-filename";
+    $command_filename = "yt-dlp -f 140 --get-filename $url";
 
     $file_name = exec(escapeshellcmd($command_filename));
 
@@ -35,10 +35,13 @@ if(!empty($_POST['url']))
 }
 ?>
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>YouTube Downloader</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <style>
     .container {
         width: 90%;
@@ -50,8 +53,8 @@ if(!empty($_POST['url']))
 <body>
     <div class="container">
         <form method="post">
-            <label>Youtube URL</label>
-            <input type="url" name="url" placeholder="https://www.youtube.com/watch?v=XXXXX" required="required">
+            <label for="url">Youtube URL</label>
+            <input type="url" name="url" id="url" placeholder="https://www.youtube.com/watch?v=XXXXX" required="required">
             <button type="submit">Download</button>
         </form>
         <?php
@@ -60,5 +63,7 @@ if(!empty($_POST['url']))
         }
         ?>
     </div>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </body>
 </html>

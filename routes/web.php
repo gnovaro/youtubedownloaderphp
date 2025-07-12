ca51d0163ee459f0090cd14b4d88cdf9d6ee9770
Gustavo Novaro <gnovaro@gmail.com>
2025-07-12 14:40:57 +0200

    Sync Laravel Basic files

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::match(['get', 'post'],'/', [App\Http\Controllers\HomeController::class, 'index']);
 
 Auth::routes([
